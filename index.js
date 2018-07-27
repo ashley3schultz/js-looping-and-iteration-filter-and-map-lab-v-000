dfec59b6cc56dd783a270e9e81ffe94d7ed057a0
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-27 03:12:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution here:
 function driversWithRevenueOver(arr, num) {
-  return arr.map(function (user) { return Object.assign({}, user)}
+  return arr.map(function (user) { if (user.revenue > num) {
+    return Object.assign({}, user)}
+  }
 }
 
 function driverNamesWithRevenueOver(arr, num) {
