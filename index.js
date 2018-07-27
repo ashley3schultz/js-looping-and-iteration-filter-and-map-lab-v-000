664a8ee5ed4686b80911743a0aa20d7fc931e3af
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-27 02:36:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver() {
+<<<<<<< HEAD
 
 }
 
@@ -14,3 +15,19 @@ function exactMatch() {
 function exactMatchToList() {
 
 }
+=======
+  
+}
+
+function driverNamesWithRevenueOver() {
+  
+}
+
+function exactMatch() {
+  
+}
+
+function exactMatchToList() {
+  
+}
+>>>>>>> 7fface8d88d948b9d0863957fcce36c5e39ec8c2
