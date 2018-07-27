cd2bf7da9954d0fa9b47b8308b7e905ac1caae69
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-27 03:05:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solution here:
 function driversWithRevenueOver(arr, num) {
-  arr.filter(users, function (user) { return user.revenue > num})
+  newarr = arr.map(function (user) {
+  return Object.assign({}, user, { equipment: 'Laptop' });
+});
 }
 
 function driverNamesWithRevenueOver(arr, num) {
-
+  arr.map(function (user) { return user.revenue: > num });
 }
 
 function exactMatch(arr, obj) {
